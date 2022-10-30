66c81b2f0c99de5953babf9b885f1c529b6bd345
LuchoD99 <luciano.diazocampo@gmail.com>
2022-10-30 18:31:29 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-// import './App.css';
+import { Store } from './Store';
+import { Container } from './components/Container';
+import { QuoteBox } from './components/QuoteBox';
+import { CreditBox } from './components/CreditBox';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Store>
+            <Container>
+                <QuoteBox />
+                <CreditBox />
+            </Container>
+        </Store>
     );
 }
 
---
+++ src/components/NewQuoteButton.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import { ACTIONS } from '../config/actions';
+import { Context } from '../Store';
+
+export function NewQuoteButton() {
+    const [state, dispatch] = useContext(Context);
+    function handleClick(e) {
+        e.preventDefault();
+        dispatch({ type: ACTIONS.UPDATE_BACKGROUND });
+        dispatch({ type: ACTIONS.UPDATE_QUOTE });
+    }
+
+    return (
+        <button
+            type="button"
+            className="btn text-white rounded-3 td-1500"
+            id="new-quote"
+            style={{ backgroundColor: state.color }}
+            onClick={handleClick}
+        >
+            <span>NEW QUOTE</span>
+        </button>
+    );
+}
